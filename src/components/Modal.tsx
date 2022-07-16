7219c4e1b51eb7293cfc9d576d20a9b85f4dce48
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-16 12:38:37 +0800

    Backdrop filter

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,15 +19,18 @@ const Modal: React.FC<PropTypes> = ({ children, title, onClose }) => {
     });
 
     return (
-        <div className="modal">
-            <div className="modal-header">
-                <h2>{title}</h2>
-                <button className="modal-close" onClick={onClose}>
-                    <img src="imgs/icons/close.svg" alt="Close Modal" />
-                </button>
+        <>
+            <div className="modal-blur" />
+            <div className="modal">
+                <div className="modal-header">
+                    <h2>{title}</h2>
+                    <button className="modal-close" onClick={onClose}>
+                        <img src="imgs/icons/close.svg" alt="Close Modal" />
+                    </button>
+                </div>
+                <div className="modal-content">{children}</div>
             </div>
-            <div className="modal-content">{children}</div>
-        </div>
+        </>
     );
 };
 
