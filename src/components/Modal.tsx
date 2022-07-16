a1e834f9c1c53ca8ddb3c3cc4f538ab1bd2865cd
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-16 12:43:10 +0800

    Modal fade in!!

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,8 +20,8 @@ const Modal: React.FC<PropTypes> = ({ children, title, onClose }) => {
 
     return (
         <>
-            <div className="modal-blur" />
-            <div className="modal">
+            <div className="modal-blur fade-in" />
+            <div className="modal fade-in">
                 <div className="modal-header">
                     <h2>{title}</h2>
                     <button className="modal-close" onClick={onClose}>
